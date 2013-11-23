77be70412bb343a498b78a9232608f94944ceb97
Andrew Volozhanin <linuxheadrus@gmail.com>
2013-11-24 05:47:50 +0600

    Refactor barbie animation using js

--- js/application.js
+++ js/application.js
@@ -1,7 +1,20 @@
 var barbieImg = document.getElementById( 'barbie_img' );
+  //barbieFront = barbieImg.getElementsByClassName( 'barbie__img__front' )[0];
 
+barbieImg.classList.add( 'barbie__img--closed' );
 
 barbieImg.addEventListener( 'click', function(){
-  barbieImg.classList.toggle( 'barbie__img--opened' );
+
+  barbieImg.classList.add( 'barbie__img--animating' );
+  //requestAnimationFrame( function(){
+    //barbieImg.classList.toggle( 'barbie__img--closing' );
+  //});
   barbieImg.title = barbieImg.title == "Open" ? "Close" : "Open";
+});
+
+barbieImg.addEventListener( 'webkitAnimationEnd', function(e){
+  barbieImg.classList.remove( 'barbie__img--animating' );
+
+  barbieImg.classList.toggle( 'barbie__img--opened' );
+  barbieImg.classList.toggle( 'barbie__img--closed' );
 });
