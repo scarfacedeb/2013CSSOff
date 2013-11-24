3eea9f89aea6531e83732a3a39fee9b1df1a3ace
Andrew Volozhanin <linuxheadrus@gmail.com>
2013-11-24 13:11:42 +0600

    Add gmaps to beatles

--- js/application.js
+++ js/application.js
@@ -1,29 +1,29 @@
-/*
-  Base
-  ========================================================================== */
-
-var transEndEventNames = {
-    'WebkitTransition' : 'webkitTransitionEnd',// Saf 6, Android Browser
-    'MozTransition'    : 'transitionend',      // only for FF < 15
-    'transition'       : 'transitionend'       // IE10, Opera, Chrome, FF 15+, Saf 7+
-  },
-  transEndEventName = transEndEventNames[ Modernizr.prefixed('transition') ],
-
-  animEndEventNames = {
-    'WebkitAnimation' : 'webkitAnimationEnd',
-    'OAnimation' : 'oAnimationEnd',
-    'msAnimation' : 'MSAnimationEnd',
-    'animation' : 'animationend'
-  },
-  animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ];
-
-
-/*
-  Barbie
-  ========================================================================== */
-
 ;(function(document, window){
   "use strict";
+
+  /*
+    Base
+    ========================================================================== */
+
+  var transEndEventNames = {
+      'WebkitTransition' : 'webkitTransitionEnd',// Saf 6, Android Browser
+      'MozTransition'    : 'transitionend',      // only for FF < 15
+      'transition'       : 'transitionend'       // IE10, Opera, Chrome, FF 15+, Saf 7+
+    },
+    transEndEventName = transEndEventNames[ Modernizr.prefixed('transition') ],
+
+    animEndEventNames = {
+      'WebkitAnimation' : 'webkitAnimationEnd',
+      'OAnimation' : 'oAnimationEnd',
+      'msAnimation' : 'MSAnimationEnd',
+      'animation' : 'animationend'
+    },
+    animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ];
+
+
+  /*
+    Barbie
+    ========================================================================== */
 
   var barbieImg = document.getElementById( 'barbie_img' ),
     //barbieFront = barbieImg.getElementsByClassName( 'barbie__img__front' )[0];
@@ -55,5 +55,46 @@ var transEndEventNames = {
 
   // set final state after on animation end
   barbieImg.addEventListener( animEndEventName, setFinalState );
+
+
+
+
+  /*
+    Beatles
+    ========================================================================== */
+
+
+  // GMaps
+
+  var beatlesMap,
+    initGMaps = function(){
+      // Enable the visual refresh
+      //google.maps.visualRefresh = true;
+
+      var mapOptions = {
+        center: new google.maps.LatLng( 51.532255, -0.177691 ),
+        zoom: 14,
+        disableDefaultUI: true,
+        styles: [{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","stylers":[{"color":"#84afa3"},{"lightness":52}]},{"stylers":[{"saturation":-17},{"gamma":0.36}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#3f518c"}]}]
+      };
+
+      beatlesMap = new google.maps.Map( document.getElementById( 'beatles_map' ), mapOptions );
+
+      // load map with beatles-related places
+      // var ctaLayer = new google.maps.KmlLayer({
+      //    url: '/TheBeatleslocations.kml'
+      //  });
+      //  ctaLayer.setMap( beatlesMap );
+    },
+
+    loadGMaps = function(){
+      var script = document.createElement( 'script' );
+      script.src = 'https://maps.googleapis.com/maps/api/js?v=3&sensor=false&region=GB&callback=initGMaps';
+      document.body.appendChild( script );
+    };
+
+  window.onload = loadGMaps;
+  window.initGMaps = initGMaps; // make it available as a callback
+
 
 })(document, window);
